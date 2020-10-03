66993dcf6b09c17d90a003c51c31e5ee62917d15
aurcode <aurelio3927@gmail.com>
2020-10-03 05:34:54 -0400

    Intera las palabras, sin animaciones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import words from './utils/words';
 import ChangeWords from './utils/ChangeWords';
 
-console.log('test')
-// setInterval
-//window.addEventListener("load", changeWords);
+const wordToChange = document.getElementById("word-to-change");
 
-setTimeout(()=>{ChangeWords(words)}, 10000)
+
+window.addEventListener('load', () => {
+    setTimeout(()=> {
+        ChangeWords(words)
+    },3000);
+    console.log("primero script")
+})
--- src/utils/ChangeWords.js
+++ src/utils/ChangeWords.js
@@ -1,6 +1,30 @@
 async function ChangeWords (words) {
-    console.log("Funciona por ahora");
-    console.log(words);
+
+    for(let i=0; i<words.length; i++) {
+        setTimeout(
+            ()=>{
+            let word = words[i];
+
+            for(let a=0; a<word.length+1; a++){
+                let dividingWord = word.slice(0, a)
+                changeDo(dividingWord)
+            }
+        }, 3000 * i)
+    }
 }
+
+
+const changeDo = async (word) => {
+    const wordToChange = null || document.getElementById("word-to-change");
+    console.log(word);
+    wordToChange.innerHTML = await Texto(word);
+
+}
+
+const Texto = (word) => {
+    const view = `<h1>I'm <span>${word}</span></h1>` ;
+    return view;
+}
+
 
 export default ChangeWords;
